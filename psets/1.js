969d26f0ee080322ebe16a6b9238eb276a6b0df2
Adams Fontin <Fresh@adamsfotinsmbp3.attlocal.net>
2016-05-16 17:22:03 -0400

    saving solution before testing.

--- psets/1.js
+++ psets/1.js
@@ -9,4 +9,16 @@ Forgot how old someone is? Calculate it!
 - Output them to the screen like so: "They are either NN or NN", substituting the values.
 */
 
-// write your solution here...
+
+
+
+var birthYear;
+
+var currentYear = prompt("What is the current year?");
+
+var birthYear = prompt("What is your birth year?");
+
+var age === currentYear - birthYear;
+
+// write your solution here..
+console.log("You are currently either" + age + "or" + (age +1) + "years old." );
