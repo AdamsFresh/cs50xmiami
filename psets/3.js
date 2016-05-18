a7165d821114645fd0b3748260f22f9ec7faf936
Adams Fontin <Fresh@Adams-Fontins-MacBook-Pro-3.local>
2016-05-17 21:34:36 -0400

    solved pset3

--- psets/3.js
+++ psets/3.js
@@ -8,5 +8,11 @@ http://math2.org/math/geometry/circles.htm
 - Calculate the circumference based on the radius, and output "The circumference is NN".
 - Calculate the area based on the radius, and output "The area is NN".
 */
+var radius = 4,
+    circumferenceCircle = 2 * Math.PI * radius;
+    areaCircle = Math.PI * Math.sqrt(radius);
 
+
+    console.log("The circumference is " + circumferenceCircle);
+    console.log("The area is " + areaCircle);
 // write your solution here...
